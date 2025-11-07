57bd8e6dae28ce48c980c5868ad8f5b4bacabf87
Hritwik Ekade <54017347+HritwikEkade@users.noreply.github.com>
2025-11-07 15:01:18 -0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('scroll', () => {
-  const header = document.querySelector('header');
-  if (window.scrollY > 50) {
-    header.style.background = 'rgba(0,0,0,0.6)';
-  } else {
-    header.style.background = 'rgba(0,0,0,0.2)';
-  }
-});
