1210bd904427b05afbbded02a0f462cc54c8770c
Hritwik Ekade <54017347+HritwikEkade@users.noreply.github.com>
2025-11-07 14:50:53 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('scroll', () => {
+  const header = document.querySelector('header');
+  if (window.scrollY > 50) {
+    header.style.background = 'rgba(0,0,0,0.6)';
+  } else {
+    header.style.background = 'rgba(0,0,0,0.2)';
+  }
+});
